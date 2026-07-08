38a8c99d351fde465584dbdfea348f32281a6151
andrefrez02 <andre.frezarini02@gmail.com>
2026-07-08 17:58:02 -0300

    fix: bug /noticias

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Não inline assets como data URI: SVGs com refs internas (url(#id))
-    // quebram quando embutidos em <img src="data:...">
+    cssMinify: false,
     assetsInlineLimit: 0,
   },
 })
